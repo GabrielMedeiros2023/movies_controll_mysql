26c2424373c9302c3283462e52e8f3d61257388f
Gabriel Medeiros <127883754+gabmedeiross@users.noreply.github.com>
2024-03-05 14:04:59 -0300

    Update atualizando tabela(update).sql

--- atualizando tabela(update).sql
+++ atualizando tabela(update).sql
@@ -3,9 +3,10 @@
 
 use cinevault;
 
-update movies set atual_ep = 12 ## atualizando o ep atual para 11
-where movies.id = 1; ## onde o id é igual a 1(da série em questão). Outra prática seria usar a coluna "name"
-
+update movies 
+set atual_ep = 12 ## atualizando o ep atual para 11
+where movies.id = 1; ## onde o id é igual a 1(da série em questão).
+  
 update movies set name = 'Avatar 2'
 where movies.id = 5
 
